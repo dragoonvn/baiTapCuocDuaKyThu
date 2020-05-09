0786695749d88e4bc43a964324b7e984f21f38e2
dragoonvn <63004209+dragoonvn@users.noreply.github.com>
2020-05-09 20:16:02 +0700

    lan 2

--- app/src/main/java/com/example/baitapcuocduakythu/MainActivity.java
+++ app/src/main/java/com/example/baitapcuocduakythu/MainActivity.java
@@ -23,9 +23,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        final CountDownTimer countDownTimer = new CountDownTimer(1000, 1000) {
+        AnhXa();
+        CountDownTimer countDownTimer = new CountDownTimer(1000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 skOne.setProgress(skOne.getProgress() + getRandom(10));
@@ -37,38 +36,31 @@ public class MainActivity extends AppCompatActivity {
             public void onFinish() {
                 if (skOne.getProgress() >= 100) {
                     Toast.makeText(MainActivity.this, "Xe 1 hạng I", Toast.LENGTH_SHORT).show();
+                    return;
                 } else if (skTwo.getProgress() >= 100) {
                     Toast.makeText(MainActivity.this, "Xe 2 hạng I", Toast.LENGTH_SHORT).show();
+                    return;
                 } else if (skThree.getProgress() >= 100) {
                     Toast.makeText(MainActivity.this, "Xe 3 hạng I", Toast.LENGTH_SHORT).show();
+                    return;
                 }
                this.start();
-
             }
         };
-        ibtnPlay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                countDownTimer.start();
-            }
-        });
-
-        AnhXa();
+        countDownTimer.start();
     }
-
     private int getRandom(int bound) {
         return new Random().nextInt(bound);
     }
-
     private void AnhXa() {
-        skOne = findViewById(R.id.seekbarOne);
-        skTwo = findViewById(R.id.seekbarTwo);
-        skThree = findViewById(R.id.seekbarThree);
-        ibtnPlay = findViewById(R.id.buttonPlay);
-        cbOne = findViewById(R.id.checkboxOne);
-        cbTwo = findViewById(R.id.checkboxTwo);
-        cbThree = findViewById(R.id.checkboxThree);
-        txtDiem = findViewById(R.id.textviewDiem);
+        skOne       = findViewById(R.id.seekbarOne);
+        skTwo       = findViewById(R.id.seekbarTwo);
+        skThree     = findViewById(R.id.seekbarThree);
+        cbOne       = findViewById(R.id.checkboxOne);
+        cbTwo       = findViewById(R.id.checkboxTwo);
+        cbThree     = findViewById(R.id.checkboxThree);
+        txtDiem     = findViewById(R.id.textviewDiem);
+        ibtnPlay    = findViewById(R.id.imgbuttonPlay);
     }
 
 }
